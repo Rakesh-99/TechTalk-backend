3e05d8886a9e9bfa8b3127e3b639d04cb414c756
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2023-10-08 23:30:27 +0530

    added express.static

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ import mongoose from 'mongoose';
 // Middleware :
 
 app.use(cors());
+app.use(express.static('public'));
 app.use(express.json());
-app.use('/', router)
+app.use('/', router);
 app.use(cookieParser)
 
 
